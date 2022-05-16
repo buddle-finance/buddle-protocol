0aba2584ed018862392f5ef2480068163ace0f96
Palash A <palash.agarwal.96@gmail.com>
2022-05-17 01:44:41 +0530

    destination rewrite

--- contracts/destination/BuddleDestOptimism.sol
+++ contracts/destination/BuddleDestOptimism.sol
@@ -1,162 +1,200 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+import "../interface/IBuddleDestination.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@eth-optimism/contracts/L1/messaging/L1CrossDomainMessenger.sol";
-import "@eth-optimism/contracts/L1/messaging/L1StandardBridge.sol";
-import "@eth-optimism/contracts/L2/messaging/L2CrossDomainMessenger.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@eth-optimism/contracts/L2/messaging/L2CrossDomainMessenger.sol";
 
-contract BuddleDestOptimism is Ownable {
+contract BuddleDestOptimism is IBuddleDestination, Ownable {
     using SafeERC20 for IERC20;
 
     address constant BASE_TOKEN_ADDRESS = address(0);
 
-    address ovmL2CrossDomainMessenger; // Address of deployed L2CrossDomainMessenger contract on Optimism
+    address messenger; // Address of deployed L2CrossDomainMessenger contract on Optimism
     address tokenBridge; // The bridge deployed on the Layer-1 chain
-    address srcContract; // Address of deployed Source Side contract on Optimism
 
-    struct TransferData {
-        address tokenAddress;
-        address destination;
-        uint256 amount;
-        uint256 fee;
-        uint256 startTime;
-        uint256 feeRampup;
-    }
-
-    mapping(uint256 => address payable) transferOwners;
+    mapping(uint256 => address payable) liquidityOwners;
     mapping(uint256 => uint256) transferFee;
-    mapping(bytes32 => bool) approvedRoot;
+    mapping(uint => mapping(bytes32 => bool)) approvedRoot;
+
+    event TransferCompleted(
+        TransferData data,
+        uint256 _id,
+        address liquidityProvider
+    );
 
 
-    /// @notice onlyL1Contract modifier
-    /// @notice only allows a message from l1DomainSideBridge contract through the L2CrossDomainMessenger bridge
-    /// @notice to call the confirmTicketPayed function
-    modifier onlyL1Contract() {
+    /* modifiers */
+
+    /**
+     * Checks whether the message sender is the L2 messenger contract
+     * and whether the message originated from the deployed L1 token bridge
+     *
+     */
+    modifier onlyBridgeContract() {
         require(
-            msg.sender == address(ovmL2CrossDomainMessenger)
-            && L2CrossDomainMessenger(ovmL2CrossDomainMessenger).xDomainMessageSender() == tokenBridge
+            msg.sender == address(messenger)
+            && L2CrossDomainMessenger(messenger).xDomainMessageSender() == tokenBridge
         );
         _;
     }
 
 
-    bool isInitialized = false;
-    function initialize(
-        address _ovmL2CrossDomainMessenger,
-        address _tokenBridge,
-        address _sourceContract
-    ) public onlyOwner {
-        require(!isInitialized, "Optimism Destination Contract already initialized!");
-        
-        ovmL2CrossDomainMessenger = _ovmL2CrossDomainMessenger;
-        tokenBridge = _tokenBridge;
-        srcContract = _sourceContract;
+    /**
+     * Checks whether the contract is initialized
+     *
+     */
+    modifier checkInitialization() {
+        require(messenger != address(0), "Contract not yet initialzied");
+        _;
     }
+
+    /* onlyOwner functions */
+
+    function initialize(
+        address _messenger,
+        address _tokenBridge
+    ) external onlyOwner {
+        require(messenger == address(0), "contract already initialized!");
+        
+        messenger = _messenger;
+        tokenBridge = _tokenBridge;
+    }
+
+    /* other functions */
 
     function changeOwner(
         TransferData memory _data,
         uint256 _transferID,
         address payable _owner
-    ) public {
-        require( (transferOwners[_transferID] == msg.sender) ||
-            (transferOwners[_transferID] == address(0) && _data.destination == msg.sender),
+    ) external checkInitialization {
+        require( (liquidityOwners[_transferID] == msg.sender) ||
+            (liquidityOwners[_transferID] == address(0) && _data.destination == msg.sender),
         "You are not authorized to call this function!");
-        transferOwners[_transferID] = _owner;
+        liquidityOwners[_transferID] = _owner;
     }
     
-    /// @notice buy function
-    /// @notice if the owner is zero, anyone can call this function paying the required tokens
-    /// @notice and claiming ownership
-    function buy(TransferData memory transferData, uint256 transferID) public payable {
-        require(transferOwners[transferID] == address(0), "Owner is non zero");
-        uint256 fee = getLPFee(transferData, block.timestamp);
+    function deposit(
+        TransferData memory transferData, 
+        uint256 transferID
+    ) external payable checkInitialization {
+        require(liquidityOwners[transferID] == address(0), "Owner is non zero");
+        
+        transferFee[transferID] = getLPFee(transferData, block.timestamp);
+        uint256 transferAmount = transferData.amount - transferFee[transferID];
+
         if (transferData.tokenAddress == BASE_TOKEN_ADDRESS) {
-            require(msg.value >= (transferData.amount - fee), "Not enough tokens sent");
+            require(msg.value >= transferAmount, "Not enough tokens sent");
+            payable(transferData.destination).transfer(transferAmount);
         } else {
-            IERC20 token = IERC20(transferData.tokenAddress);            
-            require(token.balanceOf(msg.sender) >= (transferData.amount - fee), "Insufficient token balance");
-            require(token.allowance(msg.sender, address(this)) >= (transferData.amount - fee), "Insufficient token allowance");
-            token.safeTransferFrom(msg.sender, address(this), (transferData.amount - fee));
+            IERC20 token = IERC20(transferData.tokenAddress);
+            token.safeTransferFrom(msg.sender, transferData.destination, transferAmount);
         }
-        transferFee[transferID] = fee;
-        // XXX: Added
-        transferOwners[transferID] = payable(msg.sender);
+        liquidityOwners[transferID] = payable(msg.sender);
+
+        emit TransferCompleted(transferData, transferID, liquidityOwners[transferID]);
     }
 
-    /// @notice withdraw function
-    /// @notice if the contract has enough balance, the owner(or the destination if the owner is zero)
-    /// @notice can call this function, the function will confirm the transfer with the state root
-    /// @notice and if the transfer is confirmed, the transfer value will be transfered to the 
-    /// @notice destination address
-    /// @param stateRootProof should be calculated offline
-    function withdraw(TransferData memory transferData, uint256 transferID, 
-        bytes32[] memory stateRootProof, bytes32 stateRoot) public {
+    function withdraw(
+        TransferData memory transferData,
+        uint256 transferID,
+        bytes32 _node,
+        bytes32[] memory _proof
+    ) external checkInitialization {
+        require( liquidityOwners[transferID] == msg.sender ||
+            (liquidityOwners[transferID] == address(0) && transferData.destination == msg.sender),
+            "Can only be called by owner or destination if there is no owner."
+        );
+        require(_verify(transferData, transferID, _node, _proof), 
+            "Unknown root formed from proof"
+        );
 
-        require((transferOwners[transferID] == address(0) 
-                && transferData.destination == msg.sender) ||
-                transferOwners[transferID] == msg.sender, 
-                "Can only be called by owner or destination if there is no owner.");
-
-        require(checkProof(transferData, transferID, stateRootProof, stateRoot), "Wrong state root or proof.");
-        transferOwners[transferID].transfer(transferFee[transferID]);
+        address claimer = liquidityOwners[transferID] == address(0)? 
+            transferData.destination : liquidityOwners[transferID];
+        
         if(transferData.tokenAddress == BASE_TOKEN_ADDRESS) {
-            require(transferData.amount <= address(this).balance,
-             "Contract doesn't have enough funds yet.");
+            require(address(this).balance >= transferData.amount,
+                "Contract doesn't have enough funds yet."
+            );
+            payable(claimer).transfer(transferData.amount);
 
         } else {
             IERC20 token = IERC20(transferData.tokenAddress);
-            require(transferData.amount <= token.balanceOf(address(this)),
-             "Contract doesn't have enough funds yet.");
-            token.transferFrom(address(this), transferData.destination, transferData.amount);
+            require(token.balanceOf(address(this)) >= transferData.amount,
+                "Contract doesn't have enough funds yet."
+            );
+            token.transferFrom(address(this), claimer, transferData.amount);
         }       
     }
 
-    
-    function checkProof(
-        TransferData memory _transferData, 
-        uint256 transferID, 
-        bytes32[] memory stateRootProof, 
+    function approveStateRoot(
+        uint _chain,
         bytes32 stateRoot
-    ) private view returns (bool) {
-
-        bytes32 transferDataHash = sha256(
-            abi.encodePacked(
-                _transferData.tokenAddress,_transferData.destination,
-                _transferData.amount ,_transferData.fee,
-                _transferData.startTime,_transferData.feeRampup
-            )
-        );
-        bytes32 contractAddressHash = sha256(abi.encodePacked(srcContract));
-        bytes32 nexTransferIdHash = sha256(abi.encodePacked(transferID));
-        bytes32 node = sha256(abi.encodePacked(transferDataHash, contractAddressHash,nexTransferIdHash));
-
-        bytes32 value = node;
-        for(uint n = 0; n < stateRootProof.length; n++) {
-            if(((n / (2**n)) % 2) == 1)
-                value = sha256(abi.encodePacked(stateRootProof[n], value));
-            else
-                value = sha256(abi.encodePacked(value, stateRootProof[n]));
-        }
-        return (value == stateRoot);
+    ) external onlyBridgeContract checkInitialization {
+        approvedRoot[_chain][stateRoot] = true;
     }
 
-    /// @notice getLPFee function
-    /// @notice calculates the liquidity provider fee.
-    function getLPFee(TransferData memory _transferData, uint256 _currentTime) private pure returns (uint256) {
+    /* internal functions */
+    
+    /**
+     * Verify (i) The transfer data provided matches the hash provided;
+     * (ii) The root formed from the node and proof is an approved root
+     *
+     */
+    function _verify(
+        TransferData memory _transferData, 
+        uint256 transferID, 
+        bytes32 _node,
+        bytes32[] memory _proof
+    ) internal view returns (bool) {
+
+        // Check data integrity
+        bytes32 transferDataHash = sha256(abi.encodePacked(
+            _transferData.tokenAddress,
+            _transferData.destination,
+            _transferData.amount,
+            _transferData.fee,
+            _transferData.startTime,
+            _transferData.feeRampup,
+            _transferData.chain
+        ));
+        bytes32 node = sha256(abi.encodePacked(
+            transferDataHash, 
+            sha256(abi.encodePacked(address(this))),
+            sha256(abi.encodePacked(transferID))
+        ));
+        require(node == _node, "Invalid node formed");
+
+        // Build root
+        bytes32 value = node;
+        for(uint n = 0; n < _proof.length; n++) {
+            if(((n / (2**n)) % 2) == 1)
+                value = sha256(abi.encodePacked(_proof[n], value));
+            else
+                value = sha256(abi.encodePacked(value, _proof[n]));
+        }
+        return approvedRoot[_transferData.chain][value];
+    }
+
+    /**
+     * Calculates the fees for the Liquidity Provider
+     * @notice see https://notes.ethereum.org/@vbuterin/cross_layer_2_bridges
+     * 
+     * @param _transferData The transfer metadata of the cross chain transfer
+     * @param _currentTime The current blocktime to calculate fee ramp up
+     */
+    function getLPFee(
+        TransferData memory _transferData,
+        uint256 _currentTime
+    ) internal pure returns (uint256) {
         if(_currentTime < _transferData.startTime)
             return 0;
         else if(_currentTime >= _transferData.startTime + _transferData.feeRampup)
             return _transferData.fee;
         else
             return _transferData.fee * (_currentTime - _transferData.startTime); // feeRampup
-    }
-
-    /// @notice approveStateRoot function
-    /// @notice receives the approved state root message from l1DomainSideBridge contract through the L2CrossDomainMessenger bridge
-    function approveStateRoot(bytes32 stateRoot) external onlyL1Contract{
-        approvedRoot[stateRoot] = true;
     }
 }
--- contracts/interface/IBuddleDestination.sol
+++ contracts/interface/IBuddleDestination.sol
@@ -1,2 +1,65 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.11;
+
+interface IBuddleDestination {
+
+    struct TransferData {
+        address tokenAddress;
+        address destination;
+        uint256 amount;
+        uint256 fee;
+        uint256 startTime;
+        uint256 feeRampup;
+        uint chain;
+    }
+
+    /**
+     * Initialize the contract with state variables
+     * 
+     * @param _messenger Optimism's Layer-2 Cross Domain messenger contract
+     * @param _tokenBridge Optimism's Layer-2 Standard Token Bridge contract
+     */
+    function initialize(
+        address _messenger,
+        address _tokenBridge
+    ) external;
+
+    /// @notice buy function
+    /// @notice if the owner is zero, anyone can call this function paying the required tokens
+    /// @notice and claiming ownership
+    /**
+     *
+     *
+     */
+    function deposit(
+        TransferData memory transferData, 
+        uint256 transferID
+    ) external payable;
+
+    /// @notice withdraw function
+    /// @notice if the contract has enough balance, the owner(or the destination if the owner is zero)
+    /// @notice can call this function, the function will confirm the transfer with the state root
+    /// @notice and if the transfer is confirmed, the transfer value will be transfered to the 
+    /// @notice destination address
+    /// @param _proof should be calculated offline
+    /**
+     *
+     *
+     */
+    function withdraw(
+        TransferData memory transferData,
+        uint256 transferID,
+        bytes32 _node,
+        bytes32[] memory _proof
+    ) external;
+
+    /**
+     * Approve a new root for an incoming transfer
+     * @notice only the Buddle Bridge contract on Layer 1 can call this method
+     *
+     */
+    function approveStateRoot(
+        uint _chain,
+        bytes32 stateRoot
+    ) external;
+}
