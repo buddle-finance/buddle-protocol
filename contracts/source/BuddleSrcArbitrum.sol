69aebb8c844f849c2a46a33897388f8a75b21cbc
Palash A <palash.agarwal.96@gmail.com>
2022-05-21 20:56:17 +0530

    add arbitrum src

---
+++ contracts/source/BuddleSrcArbitrum.sol
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "../abstract/BuddleSource.sol";
+
+import "arbos-precompiles/arbos/builtin/ArbSys.sol";
+import "@arbitrum/nitro-contracts/src/libraries/AddressAliasHelper.sol";
+import "arb-bridge-peripherals/contracts/tokenbridge/arbitrum/gateway/L2GatewayRouter.sol";
+
+/**
+ *
+ *
+ */
+contract BuddleSrcArbitrum is BuddleSource {
+
+    uint256 constant public CHAIN = 421611; // Arbitrum-Rinkeby
+
+    address public arbSys;
+    address public router;
+
+    mapping(address => address) public l1TokenMap; 
+
+    /********************** 
+     * onlyOwner functions *
+     ***********************/
+    
+    function setXDomainMessenger(
+        address _arbSys,
+        address _gatewayRouter
+    ) external onlyOwner checkInitialization {
+        arbSys = _arbSys;
+        router = _gatewayRouter;
+    }
+
+    function updateArbSys(address _arbSys) {
+        arbSys = _arbSys;
+    }
+
+    function updateGatewayRouter(address _gatewayRouter) {
+        router = _gatewayRouter;
+    }
+
+    function addL1Token(
+        address _l1Token,
+        address _l2Token
+    ) external onlyOwner {
+        require(tokenMapping[_l2Token], "L2 token address unknown to contract");
+        l1TokenMap[_l2Token] = _l1Token;
+    }
+
+    function updateL1Token(
+        address _l1Token,
+        address _l2Token
+    ) external onlyOwner {
+        require(l1TokenMap[_l2Token] != address(0), "Token never stored in contract");
+        l1TokenMap[_l2Token] = _l1Token;
+    }
+
+
+    /********************** 
+     * internal functions *
+     ***********************/
+
+    /**
+     * @inheritdoc BuddleSource
+     */
+    function isBridgeContract() internal view override returns (bool) {
+        return (AddressAliasHelper.undoL1ToL2Alias(msg.sender) == buddleBridge);
+    }
+
+    /**
+     * @inheritdoc BuddleSource
+     */
+    function _emitTransfer(
+        TransferData memory _data,
+        uint256 _id,
+        bytes32 _node
+    ) internal override {
+        emit TransferStarted(_data, _id, _node, CHAIN);
+    }
+
+    /**
+     * @inheritdoc BuddleSource
+     */
+    function _bridgeFunds(
+        uint256 _destChain,
+        address[] memory _tokens,
+        uint256[] memory _tokenAmounts,
+        uint256[] memory _bountyAmounts,
+        address _provider
+    ) internal override {
+
+        ArbSys _arb = ArbSys(arbSys);
+        L2GatewayRouter _router = L2GatewayRouter(router);
+
+        for (uint n = 0; n < _tokens.length; n++) {
+            if(_tokens[n] == BASE_TOKEN_ADDRESS) {
+                _arb.withdrawEth{value: _tokenAmounts[n]+_bountyAmounts[n]}(_provider);
+            } else {
+                _router.outboundTransfer(
+                    l1TokenMap(_tokens[n]),
+                    _provider,
+                    _tokenAmounts[n]+_bountyAmounts[n],
+                    bytes("")
+                );
+            }
+            tokenAmounts[_destChain][_tokens[n]] -= _tokenAmounts[n];
+            bountyAmounts[_destChain][_tokens[n]] -= _bountyAmounts[n];
+        }
+    }
+}
