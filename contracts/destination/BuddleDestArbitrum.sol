6663f3bea2456fee646f6096334bd7a50b8f2096
Palash A <palash.agarwal.96@gmail.com>
2022-05-21 21:30:58 +0530

    add arb destination

---
+++ contracts/destination/BuddleDestArbitrum.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "../abstract/BuddleDestination.sol";
+
+import "@arbitrum/nitro-contracts/src/libraries/AddressAliasHelper.sol";
+
+contract BuddleDestArbitrum is BuddleDestination {
+
+    /********************** 
+     * internal functions *
+     ***********************/
+
+    /**
+     * @inheritdoc BuddleDestination
+     */
+    function isBridgeContract() internal view override returns (bool) {
+        return (AddressAliasHelper.undoL1ToL2Alias(msg.sender) == buddleBridge);
+    }
+}
