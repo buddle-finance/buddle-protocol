b88f8b22216a49dcec135a12e8b094e4979af2b9
Palash A <palash.agarwal.96@gmail.com>
2022-05-23 07:06:27 +0530

    Boba v 0.1.0 (#10)
    * boba init
    
    * add boba interfaces
    
    * write destination boba
    
    * write source
    
    * update imports
    
    * write bridge
    
    * update boba chain id
    
    * ready to deploy

---
+++ contracts/boba/BuddleDestBoba.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "../_abstract/BuddleDestination.sol";
+
+// @dev Boba is Optimism for the most part
+// @dev see https://docs.boba.network/for-developers/developer-start#basics
+import "@eth-optimism/contracts/libraries/bridge/ICrossDomainMessenger.sol";
+
+contract BuddleDestBoba is BuddleDestination {
+
+    address public messenger;
+
+    /********************** 
+     * onlyOwner functions *
+     ***********************/
+
+    /**
+     * Change the layer-2 cross domain messenger
+     *
+     * @param _messenger Layer-2 Cross Domain messenger contract
+     */
+    function setXDomainMessenger(
+        address _messenger
+    ) external onlyOwner checkInitialization {
+        messenger = _messenger;
+    }
+
+    /**
+     * Change the layer-2 cross domain messenger
+     *
+     * @param _newMessenger Layer-2 Cross Domain messenger contract
+     */
+    function updateXDomainMessenger(
+        address _newMessenger
+    ) external onlyOwner checkInitialization {
+        messenger = _newMessenger;
+    }
+
+    /********************** 
+     * internal functions *
+     ***********************/
+
+    /**
+     * @inheritdoc BuddleDestination
+     */
+    function isBridgeContract() internal view override returns (bool) {
+        return (msg.sender == messenger && 
+            ICrossDomainMessenger(messenger).xDomainMessageSender() == buddleBridge);
+    }
+}
